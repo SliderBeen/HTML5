e322647446233c483cb6dbfc1b3d20bf71c7de25
荣刚 <363642022@qq.com>
2016-07-19 10:12:09 +0800

    Initial Commit

---
+++ JavaScriptCoreDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JavaScriptCoreDemo
+//
+//  Created by wist on 16/7/19.
+//  Copyright © 2016年 wist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
